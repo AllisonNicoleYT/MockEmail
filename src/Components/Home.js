1c34f8419fa94c49c1ac2e4b2d71923f5d3e974c
AllisonNicoleYT <68795349+AllisonNicoleYT@users.noreply.github.com>
2021-08-09 22:24:37 -0500

    Add home.css file and edited home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,11 @@ import './Home.css';
 
 //pulls in default values from app.js
 function Home(props) {
+    
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
     return (
       <div className='header__user'>
           <h2>
@@ -11,7 +16,8 @@ function Home(props) {
               {props.email}
           </div>
           <div className='header__date'>
-            {props.date.toISOString()}
+            {/*props.date.toISOString()*/}
+            <div>{month} {day}, {year}</div>            
           </div>
       </div>
     );
