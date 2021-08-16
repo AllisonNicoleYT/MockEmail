18a0d2e619fd3c1c44856bf7f449f8494327d9c8
Michael Popp <46367954+michaelpopp@users.noreply.github.com>
2021-08-15 20:07:08 -0500

    Initialized home-page branch

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components/Home';
-
+//import './App.css';
+import React, {Fragment} from "react";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Components/pages/Home';
 //created default values
-function App() {
-  const home_dummy = [
-    {
-      name: 'John Smith',
-      email: 'jsmith@txstate.edu',
-      date: new Date(2021, 7, 14),
-    },
-    {
-      name: 'Karen Omall',
-      email: 'komall@txstate.edu',
-      date: new Date(2021, 6, 22),
-    },
-    {
-    name: 'Mike Johnson',
-      email: 'mjohnson@txstate.edu',
-      date: new Date(2021, 7, 1),
-    }
-  ];
+const App = () => {
 
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Home
-        name={home_dummy[0].name}
-        email={home_dummy[0].email}
-        date={home_dummy[0].date}
-      ></Home>
-
-      <Home
-        name={home_dummy[1].name}
-        email={home_dummy[1].email}
-        date={home_dummy[1].date}
-      ></Home>
-
-      <Home
-        name={home_dummy[2].name}
-        email={home_dummy[2].email}
-        date={home_dummy[2].date}
-      ></Home>
-    </div>
-  );
-}
+    <BrowserRouter>
+    <Route exact path ='/' component = {Home}/>
+    </BrowserRouter>
+  )}
 
 export default App;
