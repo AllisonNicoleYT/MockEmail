8e3eaf272e393dd56f9dea36f7d01431850f8c42
Michael Popp <46367954+michaelpopp@users.noreply.github.com>
2021-08-17 19:15:05 -0500

    Cleaned up files directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Compose from "./pages/Compose";
